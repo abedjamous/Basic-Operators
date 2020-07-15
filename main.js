59c51e1236bae8aa766291ff41a803009bb87a66
abed jamous <jamous.hameed94@gmail.com>
2020-07-15 08:04:00 -0400

    operators in js

---
+++ main.js
@@ -0,0 +1,29 @@
+// Basic Operators
+
+let year, yearJhon, yearMark;
+now = 2020;
+ageJhon = 28;
+ageMark = 33;
+
+// Math Operators
+
+yearJhon = now - ageJhon;
+yearMark = now - ageMark;
+
+console.log(yearJhon);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical operators
+let JohenOlder = ageJhon < ageMark ;
+console.log(JohenOlder);
+
+// typeof operator
+console.log(typeof JohenOlder);
+console.log(typeof ageJhon);
+console.log(typeof "hello world");
+let x ;
+console.log(typeof x);
+
